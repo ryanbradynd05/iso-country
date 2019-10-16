6e67b969fcfbf17aa45f34a0fce786ccc2d30451
Ryan Brady <ryan.brady@bti360.com>
2019-10-16 10:54:31 -0400

    Add code to update from restcountries.eu

---
+++ src/update.ts
@@ -0,0 +1,27 @@
+function getEmoji(code) {
+	return code.toUpperCase().replace(/./g, char => String.fromCodePoint(char.charCodeAt(0)+127397) );
+}
+
+function getUnicode(str) {
+	if (str.length < 4) {
+    return str.codePointAt(0).toString(16);
+  }
+  return str.codePointAt(0).toString(16) + ' ' + str.codePointAt(2).toString(16);
+}
+
+fetch('https://restcountries.eu/rest/v2/all')
+	.then(result => result.json())
+	.then(countries => {
+		const output = countries.map((country) => {
+    	const emoji = getEmoji(country.alpha2Code);
+    	return {
+      	iso2: country.alpha2Code,
+        iso3: country.alpha3Code,
+        name: country.name,
+        localName: country.nativeName,
+        emoji,
+        emojiCode: getUnicode(emoji).toUpperCase()
+      }
+    });
+    console.log(JSON.stringify(output));
+  });
